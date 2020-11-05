5179c98e2c963461cd0a7f1e5ba9cc8aa4a2a19d
SongJeongHun <song970124@gmail.com>
2020-11-05 21:10:50 +0900

    second commit

---
+++ JHMessenger/Data/DatabaseManager.swift
@@ -0,0 +1,19 @@
+//
+//  DatabaseManger.swift
+//  JHMessenger
+//
+//  Created by 송정훈 on 2020/11/05.
+//
+
+import Foundation
+import Firebase
+class DatabaseManager{
+    static let shared = DatabaseManager()
+    private init(){
+        //Singleton 싱글톤
+    }
+    let db = Database.database().reference().child("friends")
+    var dummyList:[Friends] = [Friends(name: "김경모", comment: "안녕하세요", content: ""),Friends(name: "염종건", comment: "안녕하세요", content: "")]
+    func addFriends(){
+    }
+}
--- JHMessenger/frineds/FriendsViewcontoller.swift
+++ JHMessenger/frineds/FriendsViewcontoller.swift
@@ -6,27 +6,36 @@
 //
 
 import UIKit
+import Firebase
 
 class FriendsViewcontoller: UIViewController {
-
+    @IBOutlet weak var collectionView:UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let cell = sender as? UICollectionViewCell,let index = collectionView.indexPath(for: cell) else {return}
+        if let vc = segue.destination as? FriendsCellForViewController{
+            vc.friend = DatabaseManager.shared.dummyList[index.row]
+        }
+    }
 }
 extension FriendsViewcontoller:UICollectionViewDataSource{
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return DatabaseManager.shared.dummyList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FriendsCell", for: indexPath) as? FriendsCell else{
             return UICollectionViewCell()
         }
+        cell.friendsName.text = DatabaseManager.shared.dummyList[indexPath.row].name
+        cell.friendsComment.text = DatabaseManager.shared.dummyList[indexPath.row].comment
+        
         return cell
     }
+    
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
@@ -43,6 +52,7 @@ extension FriendsViewcontoller:UICollectionViewDataSource{
 }
 extension FriendsViewcontoller:UICollectionViewDelegate{
     
+    
 }
 extension FriendsViewcontoller:UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -58,6 +68,17 @@ class FriendsCell:UICollectionViewCell{
     @IBOutlet weak var friendsComment:UILabel!
     @IBOutlet weak var friendsThumbnail:UIImageView!
 }
+class FriendsCellForViewController:UIViewController{
+    var friend:Friends!
+    @IBOutlet weak var friendsName:UILabel!
+    @IBOutlet weak var friendsComment:UILabel!
+    @IBOutlet weak var friendsThumbnail:UIImageView!
+    override func viewDidLoad() {
+        friendsName.text = friend.name
+        friendsComment.text = friend.comment
+        super.viewDidLoad()
+    }
+}
 class FriendsHeaderCell:UICollectionReusableView{
     @IBOutlet weak var myThumbnail:UIImageView!
     @IBOutlet weak var myName:UILabel!
@@ -68,7 +89,5 @@ class FriendsHeaderCell:UICollectionReusableView{
 struct Friends:Codable{
     let name:String
     let comment:String
-    
-    
-    
+    let content:String
 }
